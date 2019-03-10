47bfd5c18722d508cb201454e84d03ae2a24782f
Tiffany Zhong <tiffzhong@gmail.com>
2019-03-10 13:57:01 -0700

    services

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
-
+import "./About.scss";
+import orange from "../Images/orange.jpg";
 class About extends Component {
   render() {
-    return <div>About</div>;
+    return (
+      <div className="about-container">
+        <section>
+          <h1>About</h1>
+          <p>
+            The Purple Tulip is a full-service floral delivery business in San
+            Jose, California, that specializes in daily deliveries, weddings,
+            special events, parties, showers, birthdays, and flowers by the week
+            for business or pleasure.
+          </p>
+
+          <div className="info">
+            <img src={orange} width="250" alt="" />
+            <article>
+              Mission: Flowers for a brighter and happier state of mind.
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </article>
+          </div>
+        </section>
+      </div>
+    );
   }
 }
 export default About;
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
-
+import "./Services.scss";
 class Services extends Component {
   render() {
-    return <div>Services</div>;
+    return (
+      <div className="services-container">
+        <section>
+          <h1>Services</h1>
+          <p>
+            We design events to help congratulate, welcome, and inspire those
+            around you, with arrangements and designs for weddings, corporate
+            and personal parties, gallery openings, and many other events.
+            Whatever your event and whatever theme you want to create - we are
+            ready and excited to get started on planning!
+          </p>
+
+          <div className="service-images">
+            <article>
+              <h4>Weddings</h4>
+              <img src="" alt="weddings" />
+            </article>
+            <article>
+              <h4>Daily Arrangements</h4>
+              <img src="" alt="daily" />
+            </article>
+            <article>
+              <h4>Special Events</h4>
+              <img src="" alt="events" />
+            </article>
+          </div>
+        </section>
+      </div>
+    );
   }
 }
 export default Services;
