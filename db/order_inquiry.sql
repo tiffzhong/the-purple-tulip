9b17dc44d06f46f8924d35b5ea2a1e1d84ba0a50
Tiffany Zhong <tiffzhong@gmail.com>
2019-03-16 12:36:47 -0700

    sql

--- db/order_inquiry.sql
+++ db/order_inquiry.sql
@@ -1,3 +1,3 @@
-insert into inquiries (id, fullname, email, inquiry, date, location, details) 
-values ($1, $2, $3, $4, $5, $6, $7);
-select * from inquiries where id = $1;
+insert into inquiries (fullname, email, inquiry, date, location, details) 
+values ($1, $2, $3, $4, $5, $6);
+select * from inquiries;
